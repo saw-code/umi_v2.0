172e631abc1f7afb545be700b6ee1aee06772f30
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-05-18 16:33:59 +0300

    57_page

---
+++ src/types/analytics.d.ts
@@ -0,0 +1,14 @@
+export interface TopOpportunity {
+  name: string;
+  revenue: string;
+}
+export interface LeadsSource {
+  source: string;
+  count: number;
+  percent: number;
+}
+export interface HistoryByMonth {
+  name: string;
+  month: string;
+  value: string;
+}
