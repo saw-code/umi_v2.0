19342961fc84bcdaee4535b0ccefda8d5134ede9
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-05-21 21:31:08 +0300

    62_page

--- src/pages/Customers/index.tsx
+++ src/pages/Customers/index.tsx
@@ -3,17 +3,20 @@ import { Button } from 'antd';
 import ProTable from '@ant-design/pro-table';
 import { FormattedMessage, getLocale } from 'umi';
 import { PageContainer } from '@ant-design/pro-layout';
+import columns from './columns';
+import {Customer} from "@/types/customer";
 
 export default function Page() {
   return (
     <PageContainer style={{ minHeight: '90vh' }}>
-      <ProTable<any>
+      <ProTable<Customer>
         rowKey="id"
         headerTitle={<FormattedMessage id="table.customer.title"/>}
         search={{ labelWidth: 'auto' }}
         pagination={{ pageSize: 5 }}
         dateFormatter="string"
         locale={getLocale()}
+        columns={columns}
         toolBarRender={() => [
           <Button key="button" icon={<PlusOutlined />} type="primary">
             <FormattedMessage id="table.new" />
